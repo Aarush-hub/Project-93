ae5e4766be00d494d729192dabb2e72a0a516ac6
Aarush-hub <81354199+Aarush-hub@users.noreply.github.com>
2021-12-04 16:18:29 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC3lTZZbcmA0qvcCsl2X9B0dUM3GRUOGdQ",
+    authDomain: "lets-chat-web-app-4a7ca.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-4a7ca-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-4a7ca",
+    storageBucket: "lets-chat-web-app-4a7ca.appspot.com",
+    messagingSenderId: "575855192100",
+    appId: "1:575855192100:web:d5b24e626f873362e750b1"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
